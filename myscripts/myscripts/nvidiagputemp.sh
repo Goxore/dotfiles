db4d61595fa22b0fa5f7b6fb99698da10f7ca490
Goxore <ggooxxoorree@gmail.com>
2022-01-02 16:50:06 +0200

    gpu temp fix

--- myscripts/myscripts/nvidiagputemp.sh
+++ myscripts/myscripts/nvidiagputemp.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-command -v nvidia-smi && nvidia-smi | grep '[0-9][0-9]C' | awk '{print $3}' | sed 's/C//'
+#command -v nvidia-smi && nvidia-smi | grep '[0-9][0-9]C' | awk '{print $3}' | sed 's/C//'
+
+command -v nvidia-smi >/dev/null && nvidia-smi | grep '[0-9][0-9]C' | awk '{print $3}' | sed 's/C//'
